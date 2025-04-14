94eddf7f607f26a2c910ded6790d70b365586578
Dilki Fernando <dfnisansala@virtusa.com>
2025-04-14 19:19:22 +0530

    Initial Commit - OrangeHRM Automation

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	
+	WebDriver driver;
+	
+	By username = By.xpath("//input[@name=\"username\"]");
+	By password = By.xpath("//input[@name=\"password\"]");
+	By loginBtn = By.xpath("//button[@type=\"submit\"]");
+	
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void enterCredentials(String userName, String Password) {
+		driver.findElement(username).sendKeys(userName);
+		driver.findElement(password).sendKeys(Password);
+		
+	}
+	
+	public void clickLogin() {
+		driver.findElement(loginBtn).click();
+		
+	}
+
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+
+public class LoginTest {
+	
+	WebDriver driver;
+	LoginPage loginpage;
+	
+	@BeforeMethod
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		loginpage = new LoginPage(driver);
+	}
+	
+	@Test
+	public void validLogin() {
+		loginpage.enterCredentials("Admin", "admin123");
+		loginpage.clickLogin();
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+}
